3da551dbd29ce6ff0a1e54f4433dd05789653310
AlexPs06 <luis_pesar@hotmail.com>
2019-07-03 00:37:09 -0500

    envio de notificaicon en progreso problema de cors

--- admin/src/components/HelloWorld.vue
+++ admin/src/components/HelloWorld.vue
@@ -8,8 +8,8 @@
         <td>{{ pedidos.item.direccion }}</td>
         <td>{{ pedidos.item.referencias }}</td>
         <td>{{ pedidos.item.telefono }}</td>
-        <td>{{ ordenes[pedidos.item.id-1] }}</td>
-        <td>{{ pedidos.item.token }}</td>
+        <!-- <td>{{ ordenes[pedidos.item.id-1] }}</td> -->
+        <!-- <td>{{ pedidos.item.token }}</td> -->
         <td>
           <div class="conteiner">
             <v-select
@@ -28,7 +28,7 @@
 
 <script>
 import axios from 'axios';
-
+import request from 'request'
 let api = "http://127.0.0.1:3333/api/v1"
 
 export default {
@@ -62,13 +62,12 @@ export default {
         //this.pizzas = response.data
         this.pedidos = response.data
         response.data.forEach(element => {
-            console.log(element.tokenNotificaciones)
             this.estatus.push(element.estatus)
             let elementos = JSON.parse(element.lista)
             let orden = "Pizza: " + elementos[0].nombre + ", Ingredientes: " + elementos[0].ingredientes + ", Cantidad: " + elementos[0].cantidadPizzas
             this.ordenes.push(orden)
-            console.log(JSON.parse(element.lista))
         });
+        
         
         
         //console.log("Hola", JSON.parse(response.data[0].lista))
@@ -76,12 +75,61 @@ export default {
       })
     },
     methods:{
+
+      
         changeStatus(pedido){
+        console.log(this.pedidos[0].tokenNotificaciones)
+
+          let data = {
+              "notification": {
+                "title": "FCM Message",
+                "body": "This is an FCM Message",
+                "icon": "./img/icons/android-chrome-192x192.png"
+              }
+            }
+          let notification= {
+                title: "FCM Message",
+                body: "This is an FCM Message",
+                icon: "./img/icons/android-chrome-192x192.png"
+              }
+          const options = {
+          method: 'POST',
+          url: 'https://fcm.googleapis.com/fcm/send',
+          headers: {
+          'Authorization':"key=AAAASim8sYY:APA91bHyE3EdrFabmRy-Lsi2DhWeIRGTPzvS9T638Qee80ah65rST4NOcnbQRsNFyLkZW_sLRdiCOCds_7VwI8l9tNmpJhlncIi1r4_NDSLscLY1cpnYGKw7XmPTwolMtW6dq347F5LK",
+          'Content-Type': "application/json",
+          "Access-Control-Allow-Origin":"http://192.168.0.12:8080/",
+          to: this.pedidos[0].tokenNotificaciones,
+
+          
+          },
+          body: notification,
+          json: true // JSON stringifies the body automatically
+          }
+          
+          request(options)
+          // .then(function (response) {
+          // // Handle success response data
+          // })
+
+          /*
+          curl -X POST -H "Authorization: key=AAAASim8sYY:APA91bHyE3EdrFabmRy-Lsi2DhWeIRGTPzvS9T638Qee80ah65rST4NOcnbQRsNFyLkZW_sLRdiCOCds_7VwI8l9tNmpJhlncIi1r4_NDSLscLY1cpnYGKw7XmPTwolMtW6dq347F5LK" -H "Content-Type: application/json" -d '{
+              "to": this.pedidos[0].tokenNotificaciones+"",
+              "notification": {
+                "title": "FCM Message",
+                "body": "This is an FCM Message",
+                "icon": "./img/icons/android-chrome-192x192.png"
+              }
+            }' https://fcm.googleapis.com/fcm/send
+          
+          */
+
+
           pedido.estatus = this.estatus[pedido.id-1]
           console.log(pedido)
-          axios.put(api + "/compras/" + pedido.id, pedido ).then((response) => {
-            console.log(response)
-          })
+          // axios.put(api + "/compras/" + pedido.id, pedido ).then((response) => {
+          //   console.log(response)
+          // })
         }
     }
   
