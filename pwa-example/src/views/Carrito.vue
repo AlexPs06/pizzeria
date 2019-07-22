025c2d19dc627de18bc158cccbe4c8897a37e15a
AugustoRucle <augustorucle@gmail.com>
2019-07-22 11:49:48 -0500

    Agrege parche carrito

--- pwa-example/src/views/Carrito.vue
+++ pwa-example/src/views/Carrito.vue
@@ -1,11 +1,10 @@
 <template>
-    <div class="container">
-        <div v-for="orden in ordenes " v-bind:key = "orden.id">
-      
+  <div class="container" v-if="ordenes != null">
+    <div v-for="orden in ordenes " v-bind:key = "orden.id">
       <v-flex xs12 sm8 offset-sm2 >
         <v-card>
           <v-img v-bind:src= "orden.imagen" aspect-ratio="2.75"></v-img>
-
+          
           <v-card-title primary-title>
             <div>
               <h3 class="headline mb-0">{{orden.nombre}}</h3>
@@ -14,47 +13,40 @@
               <span class="green--text">Precio: {{orden.precio}}</span>
             </div>
           </v-card-title>
-
-
         </v-card>
         <br>
       </v-flex>
-      
     </div>
-    <v-btn
-    
-    color="info"
-    v-on:click="borrarCarrito()"
-   
-    >Borrar carrito
-    </v-btn>
-     
-    <v-btn
-    color="info"
-    style="margin-left: 800px"
-    :to="{name:'pagos'}"
-    >Pagar
-    </v-btn>
-    </div>
-
+    <v-btn  color="info" v-on:click="borrarCarrito()">Borrar carrito</v-btn>
+    <v-btn color="info" style="margin-left: 800px" :to="{name:'pagos'}" >Pagar</v-btn>
+  </div>
+  <div v-else>
+    <v-container bg fill-height grid-list-md text-xs-center>
+      <v-layout row wrap align-center>
+        <v-flex>
+          I don't have data.
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
 import Vue from 'vue'
-
-      let carrito = JSON.parse(localStorage.getItem('carrito')+"")
-
+  let carrito = JSON.parse(localStorage.getItem('carrito')+"")
 export default {
   data() {
     return {
-        ordenes: [],
+      ordenes: null,
     }
   }, created() {
-      
+    if(this.ordenes != null){
+      this.ordenes = []
       console.log("Carro", carrito)
       carrito.forEach(element => {
           this.ordenes.push(element)
-      });
+      });      
+    }
   }, methods:{
       borrarCarrito(){
           localStorage.clear()
